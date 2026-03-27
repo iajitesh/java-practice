5f8d981c6d9373143a650a8792229b9af321b8f0
Ajitesh Verma <ajitesh.verma24@gmail.com>
2026-03-27 13:21:39 +0530

    Added solutions for Buy and Sell Stock and Trapping Rain Water array problems

---
+++ Arrays/BuyAndSellStock.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class BuyAndSellStock {
+    public static void BuyAndSell(int[] prices){
+      int buyStock = Integer.MAX_VALUE;
+      int maxProfit = 0;
+
+      for(int i=0; i<prices.length; i++){
+        if(buyStock < prices[i]){
+            int Profit = prices[i] - buyStock; // Aaj ka profit
+            maxProfit = Math.max(maxProfit, Profit);
+        }else{
+            buyStock = prices[i]; //jis din kam hai us din khareed liya!
+        }
+      }
+      System.out.println("Max profit is: "+ maxProfit);
+
+    }
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        BuyAndSell(prices);
+    }
+}
